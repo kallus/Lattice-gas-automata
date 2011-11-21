89b6bcefbebeb12b3708f50a975c3fdf423d25ed
Joel Wilsson <joel.wilsson@gmail.com>
2011-11-21 18:13:07 +0100

    Initial commit.

---
+++ lattice_model.py
@@ -0,0 +1,135 @@
+import numpy as np
+
+N_STATES = 15
+class LatticeModel(object):
+    def __init__(self, lattice_size, n_particles):
+        self.size = lattice_size
+        self.cells = np.zeros((self.size, self.size), dtype=np.int)
+        self.cells_next = np.zeros((self.size, self.size), dtype=np.int)
+        for i in xrange(n_particles):
+            row = random.randint(0, lattice_size)
+            col = random.randint(0, lattice_size)
+            while self.cells[row, col] <> 0:
+                row = random.randint(0, lattice_size)
+                col = random.randint(0, lattice_size)
+            self.cells[row, col] = random.randint(0, N_STATES - 1)
+
+    def update(self):
+        # collision step.
+        for row in xrange(self.lattice_size):
+            for col in xrange(self.lattice_size):
+                if self.cells[row, col] == 0:
+                    #  .
+                    # . .
+                    #  .
+                    # do nothing.
+                    continue
+                elif self.cells[row, col] == 1:
+                    #  ↓
+                    # . .
+                    #  .
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 2:
+                    #  .
+                    # . ←
+                    #  .
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 3:
+                    #  ↓
+                    # . ←
+                    #  .
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 4:
+                    #  .
+                    # . .
+                    #  ↑
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 5:
+                    #  .
+                    # → .
+                    #  .
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 5:
+                    #  ↓
+                    # . .
+                    #  ↑
+                    # => (10)
+                    #  .
+                    # → ←
+                    #  .
+                    # collision.
+                    self.cells[row, col] == 10
+                elif self.cells[row, col] == 6:
+                    #  .
+                    # . ←
+                    #  ↑
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 7:
+                    #  ↓
+                    # . ←
+                    #  ↑
+                    # collides, but maps to itself.
+                    continue
+                elif self.cells[row, col] == 8:
+                    #  .
+                    # → .
+                    #  .
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 9:
+                    #  ↓
+                    # → .
+                    #  .
+                    # collides, but maps to itself.
+                    continue
+                elif self.cells[row, col] == 10:
+                    #  .
+                    # → ←
+                    #  .
+                    # => (5)
+                    #  ↓
+                    # . .
+                    #  ↑
+                    # collision.
+                    self.cells[row, col] = 5
+                elif self.cells[row, col] == 11:
+                    #  ↓
+                    # → ←
+                    #  .
+                    # no collision.
+                    continue
+                elif self.cells[row, col] == 12:
+                    #  .
+                    # → .
+                    #  ↑
+                    # collides, but maps to itself.
+                    continue
+                elif self.cells[row, col] == 13:
+                    #  ↓
+                    # → .
+                    #  ↑
+                    # collides, but maps to itself.
+                    continue
+                elif self.cells[row, col] == 14:
+                    #  .
+                    # → ←
+                    #  ↑
+                    # collides, but maps to itself.
+                    continue
+                elif self.cells[row, col] == 15:
+                    #  ↓
+                    # → ←
+                    #  ↑
+                    # collides, but maps to itself.
+                    continue
+
+        # propagation step.
+        for row in xrange(self.lattice_size):
+            for col in xrange(self.lattice_size):
+
---
+++ lattice_widget.py
@@ -0,0 +1,32 @@
+from PySide import QtCore, QtGui
+
+class LatticeWidget(QtGui.QWidget):
+    def __init__(self, lattice_model, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        self.lattice_model = lattice_model
+
+    def paintEvent(self, event):
+        painter = QtGui.painter()
+        diameter = (self.width() / float(self.lattice_model.size))
+        
+        painter.begin(self)
+        painter.fillRect(event.rect(), QtCore.Qt.white)
+        painter.end()
+
+        for row in xrange(self.lattice_model.size):
+            for col in xrange(self.lattice_model.size):
+                if self.lattice_model.cells[row, col] <> 0:
+                    painter.begin(self)
+                    painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
+                    pen = painter.pen()
+                    pen.setColor(QtCore.Qt.black)
+                    pen.setBrush(QtCore.Qt.black)
+                    painter.setPen(pen)
+                    painter.drawEllipse(QtCore.QRect(-diameter/2.0, -diameter/2.0, diameter, diameter))
+                    painter.end()
+                    
+    def minimumSizeHint(self):
+        return QtCore.QSize(200, 200)
+
+    def sizeHint(self):
+        return QtCore.QSize(400, 400)
