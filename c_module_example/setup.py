ee650dd5ee3e830e037a4c5ec770d30d107820bd
Joel Wilsson <joel.wilsson@gmail.com>
2011-11-21 16:59:00 +0100

    Add comments.

--- c_module_example/setup.py
+++ c_module_example/setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
 import numpy as np
 
+# to run:
+# python setup.py build
+# cp build/<something>/c_module.so .
+# python c_module_example.py
 module = Extension('c_module', sources = ['c_module.c'])
 setup(name = 'C module example',
       version = '1.0',
